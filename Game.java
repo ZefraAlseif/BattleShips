efdc0d132d98af98366911c34249dbf13908353a
ZefraAlseif <carlosotero9819@gmail.com>
2023-06-15 21:33:06 -0400

    Added the ability for players to pick where there ships would go.

--- Game.java
+++ Game.java
@@ -1,8 +1,35 @@
-
+import java.util.Scanner;
 
 public class Game{
     public static void main(String[] args) {
         Logic game_start = new Logic();
-        game_start.setShips(4, 5, 2, null, 0); 
+        Scanner sc = new Scanner(System.in);
+        boolean running = true; // Used for each of the keypoints
+        int count = 0;
+        int ship_num = 0; 
+        while(count < 5){
+            System.out.println("Input the row where your ship would go: ");
+            int row = sc.nextInt();
+            System.out.println("Input the col where your ship would go: ");
+            int col = sc.nextInt();
+            System.out.println("Input the size of the ship (between 2-9): ");
+            int size = sc.nextInt();
+            int temp = game_start.checkSetShips(row,col,size,String.valueOf(ship_num),1); 
+            count += temp;
+            ship_num += temp;
+        }
+        count = 0;
+        ship_num = 0; 
+        while(count < 5){
+            System.out.println("Input the row where your ship would go: ");
+            int row = sc.nextInt();
+            System.out.println("Input the col where your ship would go: ");
+            int col = sc.nextInt();
+            System.out.println("Input the size of the ship (between 2-9): ");
+            int size = sc.nextInt();
+            int temp = game_start.checkSetShips(row-1,col-1,size,String.valueOf(ship_num),2); 
+            count += temp;
+            ship_num += temp;
+        }
     }
 }
--- Logic.java
+++ Logic.java
@@ -2,10 +2,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Logic {
-    private ArrayList<ArrayList<String>> p1_field;
-    private ArrayList<ArrayList<String>> p2_field;
-    private ArrayList<String> empty_field;
-    private HashMap<Integer, ArrayList<ArrayList<String>>> player_map;
+    private String [][] p1_field;
+    private String [][] p2_field;
+    private HashMap<Integer, String [][]> player_map;
     /*
      * List<List<String>> = [
      * []
@@ -14,28 +13,44 @@ public class Logic {
      * []
      *
      */
-
     // Create the Starting empty List
     Logic() {
-        p1_field = new ArrayList<ArrayList<String>>();
-        p2_field = new ArrayList<ArrayList<String>>();
-        empty_field = new ArrayList<String>(10);
-        for (int i = 0; i < 10; i++) {
-            empty_field.add("--");
+        p1_field = new String[10][10];
+        p2_field = new String[10][10];
+        for (int row = 0; row < 10; row++) {
+            for (int col =0; col < 10; col++){
+                p1_field[row][col] = "--";
+                p2_field[row][col] = "--";
+
+            }  
         }
-        for (int i = 0; i < 10; i++) {
-            p1_field.add(empty_field);
-            p2_field.add(empty_field);
-        }
-        player_map = new HashMap<Integer, ArrayList<ArrayList<String>>>();
+        System.out.println(p1_field);
+        player_map = new HashMap<Integer, String [][]>();
         player_map.put(1, p1_field);
         player_map.put(2, p2_field);
     }
 
     public void setShips(int row, int col, int size, String ship_num, int player_num) {
-        player_map.get(player_num).get(row).set(col, ship_num);
-        while (size > 0) {
-            player_map.get(player_num).get(row).set(col, ship_num);
+        while (size > 0){
+            player_map.get(player_num) [row][col+size] = ship_num;
+            size--;
+        }
+        for (String[] strings : p1_field) {
+            for (String string : strings) {
+                System.out.print(string);
+            }
+            System.out.println("");    
+        }
+    }
+
+    public int checkSetShips(int row, int col, int size, String ship_num, int player_num){
+        if (col + size < 10){
+            setShips(row, col, size, ship_num, player_num);
+            return 1;
+        }
+        else{
+            System.out.println("Location chosen does not work");
+            return 0;
         }
     }
 }
